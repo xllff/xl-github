8af71772253c36013aa6666030d2fe4b4bf8198e
dainslef <dainslef@live.cn>
2015-08-08 05:35:45 +0200

    all file

---
+++ Python/模块.py
@@ -0,0 +1,19 @@
+﻿#模块是函数功能的扩展，是多个函数的集合
+
+
+#math库，常用数学计算库
+
+#import math
+
+#print math.pi
+
+from math import *
+
+print pi
+
+#sys模块,标准库模块，也与系统功能有关，在Python安装中就已存在
+import sys
+print sys.version
+print sys.executable #查看运行的程序
+print sys.getwindowsversion()
+print sys.modules.keys() #查看已导入的关键字
